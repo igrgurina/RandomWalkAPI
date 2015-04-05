a8da6bfcbe1433ebc9eb184fb7231c01d7522ccf
Ivan Grgurina <ivan.grgurina@gmail.com>
2015-04-05 22:25:10 +0200

    Create INode.cs

---
+++ INode.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWalk {
+
+    /// <summary>
+    /// Graph node interface
+    /// </summary>
+    public interface INode {
+
+        public double Price { get; set; }
+
+    }
+
+}
